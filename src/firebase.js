8e86dea48062bd8cca4b1179f5bec7bfcac88a94
Amjad Shomman <Amjadshomman@Amjads-MacBook-Pro-5.local>
2025-04-17 02:47:54 +0300

    Add firebase.js and package.json scripts

--- src/firebase.js
+++ src/firebase.js
@@ -16,3 +16,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+
+"scripts": {
+  "build": "react-scripts build"
+}
